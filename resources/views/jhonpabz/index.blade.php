e27f22a2c9a2595e95e53df1accff4804b502c67
jhonpabz <jhncsrpbl@gmail.com>
2025-02-01 13:19:01 +0800

    add: network route and page

---
+++ resources/views/jhonpabz/index.blade.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JhonPabz Network | Home</title>
+</head>
+<body>
+    <h2>Currently Available Network</h2>
+
+    <ul>
+        <li>Jhonpabz Gaming!</li>
+    </ul>
+</body>
+</html>
