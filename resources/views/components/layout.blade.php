641ae4e9308703ca96dc57c9cf6dca5501dfbc8f
jhonpabz <jhncsrpbl@gmail.com>
2025-02-03 12:25:55 +0800

    add: layout component

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,25 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jhon Network</title>
+</head>
+
+<body>
+
+    <header>
+        <nav>
+            <h1>Jhon Network</h1>
+            <a href="/jhonpabz">All Networks</a>
+            <a href="/jhonpabz/create">Create New Network</a>
+        </nav>
+    </header>
+
+    <main class="container">
+        {{ $slot }}
+    </main>
+
+</body>
+
+</html>
