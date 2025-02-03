f5a7fcc511cb89441476e3446beb3f2b8b8e4115
jhonpabz <jhncsrpbl@gmail.com>
2025-02-03 19:05:45 +0800

    add: table column

--- database/migrations/2025_02_03_100930_create_jhonpabzs_table.php
+++ database/migrations/2025_02_03_100930_create_jhonpabzs_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('jhonpabzs', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->integer('skill');
+            $table->text('bio');
         });
     }
 
