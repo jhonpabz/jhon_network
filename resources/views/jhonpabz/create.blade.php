fe1d2f4acb904fc05e9eb16d9376eeaace1538ff
jhonpabz <jhncsrpbl@gmail.com>
2025-02-06 19:33:39 +0800

    add: create form

--- resources/views/jhonpabz/create.blade.php
+++ resources/views/jhonpabz/create.blade.php
@@ -1,3 +1,33 @@
 <x-layout>
-    <h1>Create Test</h1>
+    <form action="" method="">
+        <h2>Create a New Jhonpabz</h2>
+
+        <!-- Jhonpabz Name -->
+        <label for="name">Jhonpabz Name:</label>
+        <input type="text" id="name" name="name" value="{{ old('name') }}" required>
+
+        <!-- Jhonpabz Strength -->
+        <label for="skill">Jhonpabz Skill (0-100):</label>
+        <input type="number" id="skill" name="skill" required>
+
+        <!-- Jhonpabz Bio -->
+        <label for="bio">Biography:</label>
+        <textarea rows="5" id="bio" name="bio" required></textarea>
+
+        <!-- select a network -->
+        <label for="network_id">Network:</label>
+        <select id="network_id" name="network_id" required>
+            <option value="" disabled selected>Select a network</option>
+            @foreach ($network as $item)
+                <option value="{{ $item->item }}">{{ $item->name }}</option>
+            @endforeach
+
+        </select>
+
+        <button type="submit" class="btn mt-4">Create Jhonpabz</button>
+
+        <!-- validation errors -->
+
+    </form>
+
 </x-layout>
