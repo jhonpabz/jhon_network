c4d9720a68197eff839812cc0dcf1faed242d272
jhonpabz <jhncsrpbl@gmail.com>
2025-02-06 17:18:57 +0800

    display related network

--- resources/views/jhonpabz/index.blade.php
+++ resources/views/jhonpabz/index.blade.php
@@ -5,7 +5,10 @@
         @foreach ($jhonpabz as $item)
             <li>
                 <x-card href="{{ route('jhonpabz.show', $item->id) }}" :highlight="$item->skill > 75">
-                    <h3>{{ $item->name }}</h3>
+                    <div>
+                        <h3>{{ $item->name }}</h3>
+                        <p>{{ $item->network->name }}</p>
+                    </div>
                 </x-card>
             </li>
         @endforeach
--- resources/views/jhonpabz/show.blade.php
+++ resources/views/jhonpabz/show.blade.php
@@ -4,4 +4,12 @@
     <p><strong>About me:</strong></p>
     <p>{{ $jhonpabz->bio }}</p>
 
+    <div class="border-2 border-dashed bg-white px-4 pb-4 my-4 rounded">
+        <h3>Netowork Information</h3>
+        <p><strong>Network name:</strong>{{ $jhonpabz->network->name }}</p>
+        <p><strong>Location:</strong>{{ $jhonpabz->network->location }}</p>
+        <p><strong>About the Network:</strong></p>
+        <p></p>{{ $jhonpabz->network->description }}</p>
+    </div>
+
 </x-layout>
