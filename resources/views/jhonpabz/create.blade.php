df551c2d7dd0a779525282e7f732d8b4599790a4
jhonpabz <jhncsrpbl@gmail.com>
2025-02-07 17:28:32 +0800

    display error message

--- resources/views/jhonpabz/create.blade.php
+++ resources/views/jhonpabz/create.blade.php
@@ -31,6 +31,15 @@
         <button type="submit" class="btn mt-4">Create Jhonpabz</button>
 
         <!-- validation errors -->
+        @if ($errors->any)
+            <ul class="px-4 py-2 bg-red-100">
+                @foreach ($errors->all() as $error)
+                    <li class="my-2 text-red-500">
+                        {{ $error }}
+                    </li>
+                @endforeach
+            </ul>
+        @endif
 
     </form>
 
