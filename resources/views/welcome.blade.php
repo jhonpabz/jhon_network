35c1f46caee1e71c9301347a1ca9ad7614565325
jhonpabz <jhncsrpbl@gmail.com>
2025-02-10 13:02:58 +0800

    separate the banner swipper and move it to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,10 +6,17 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>JhonPabz Network</title>
 
+    <!-- Add Swiper's CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
     @vite('resources/css/app.css')
+
+    <!-- Add Swiper JS -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 </head>
 
 <body class="text-center px-8 py-12">
+    <x-banner-swipper />
     <h1 class="mb-8">JhonPabz Network</h1>
     <a href="/jhonpabz" class="btn">Lets Go!</a>
 </body>
